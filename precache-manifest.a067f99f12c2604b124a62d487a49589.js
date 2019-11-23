763ed91ff96b68ae2defff69200b75d8e3f33fa1
TomeQ <tomeq1888@gmail.com>
2019-11-23 20:12:50 +0100

    Updates

---
+++ precache-manifest.a067f99f12c2604b124a62d487a49589.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a9ba9fa5c2f8521c6ae3cf4e0d6fbe",
+    "url": "/tomek/index.html"
+  },
+  {
+    "revision": "485eb5437027c2b56594",
+    "url": "/tomek/static/css/main.fe7a4c78.chunk.css"
+  },
+  {
+    "revision": "0dffe7ce9d1ba58821cf",
+    "url": "/tomek/static/js/2.0997baeb.chunk.js"
+  },
+  {
+    "revision": "485eb5437027c2b56594",
+    "url": "/tomek/static/js/main.3ec4f014.chunk.js"
+  },
+  {
+    "revision": "b1523668df04814151a2",
+    "url": "/tomek/static/js/runtime-main.d8810780.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tomek/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "7dcbaaed09f4e0ecec28d6a2ca81dfbb",
+    "url": "/tomek/static/media/yt-logo.7dcbaaed.png"
+  }
+]);
